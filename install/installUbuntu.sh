21b054cc1494d066cbb89901a87d6ff0869a7549
Joshua-Micheletti <joshua.e.n.micheletti@gmail.com>
2023-02-15 22:40:26 +0100

    updated linux install script

--- install/installUbuntu.sh
+++ install/installUbuntu.sh
@@ -6,6 +6,7 @@ sudo apt install python3-tk
 #python3 -m pip install git+https://github.com/RedFantom/ttkthemes
 pip install ttkthemes
 pip install tkcalendar
+pip install matplotlib
 
 echo "#!/bin/bash" > ../launch.sh
 echo python3 ./src/app.py > ../launch.sh
